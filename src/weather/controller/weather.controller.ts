a0172e8440b98219dd6eeaaf318b2aabadad216c
gustavo.garozzo <gustavo.garozzo@andromedalatam.com>
2025-09-08 15:54:06 -0300

    add: swagger configuration

--- src/weather/controller/weather.controller.ts
+++ src/weather/controller/weather.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, HttpCode, Query } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
-import { WeatherService } from '../service/weather.service';
+import { ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 import { getWeahterDto } from '../dto/getWeather.dto';
+import { WeatherService } from '../service/weather.service';
 
 @Controller('weather')
 export class WeatherController {
@@ -9,6 +9,13 @@ export class WeatherController {
 
   @Get()
   @HttpCode(200)
+  @ApiQuery({
+    name: 'city',
+    required: false,
+    type: String,
+    description: 'Name of the city to check weather',
+    example: 'Buenos Aires'
+  })
   @ApiOperation({ summary: 'Check the wheather' })
   async getWeather(@Query('city') params: getWeahterDto) {
     return this.weatherService.getWeather(params.city);
--- src/weather/dto/getWeather.dto.ts
+++ src/weather/dto/getWeather.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class getWeahterDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    type: 'string',
+    example: 'Buenos Aires',
+    description: 'City to check weather',
+    required: false
+  })
   city?: string;
 }
